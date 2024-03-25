5808d80c383ac638b67f8eccced950c995a5c89f
Ammar-Amin <ammaramin5253@gmail.com>
2024-03-25 17:10:53 +0530

    watch stories

--- script.js
+++ script.js
@@ -1,16 +1,51 @@
 let body = document.querySelector('body');
-let stories = document.querySelectorAll('.stories-img');
+let stories = document.querySelector('.stories');
+let storyBg = document.querySelector('.storyBg');
+let clickedStory = document.querySelector('.wStory')
 
-stories.forEach((story, i) => {
-    i = i + 1;
-    story.addEventListener('click', (e) => {
-        console.log(e.target.src)
-        console.log('story', i);
-        let div = document.createElement('div');
-        div.style.position = 'absolute';
-        div.style.width = '100%';
-        div.style.height = '100%';
-        div.style.backgroundColor = '#dadada';
-        body.appendChild(div);
-    })
+stories.addEventListener('click', function (e) {
+    // console.log(e.target);
+    if (e.target.classList.contains('img')) {
+        // console.log('hello')
+        storyBg.style.display = 'block';
+        clickedStory.opacity = 1;
+        clickedStory.src = e.target.src;
+    }
+
+    setTimeout(() => {
+        storyBg.style.display = 'none';
+    }, 2000)
 })
+
+
+function likePost() {
+    let mainPost = document.querySelector('.main-post')
+    let click = false;
+    mainPost.addEventListener('click', (e) => {
+        // console.log(e.target)
+        if (e.target.classList.contains('like')) {
+            click = !click
+            if (click) {
+                e.target.style.backgroundColor = 'red'
+            } else {
+                e.target.style.backgroundColor = 'white'
+            }
+        }
+    })
+}
+likePost();
+
+
+// let likeIcons = document.querySelectorAll('.like');
+// let click = false;
+// likeIcons.forEach(like => {
+//     like.addEventListener('click', () => {
+//         console.log('click')
+//         click = !click;
+//         if (click) {
+//             like.style.backgroundColor = 'red'
+//         } else {
+//             like.style.backgroundColor = 'white'
+//         }
+//     })
+// });
